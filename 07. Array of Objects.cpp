cb8210048fc7b23843b99247c9c81aede3f6761e
11249a268 <11249a268@kanchiuniv.ac.in>
2025-11-20 22:09:37 +0530

    Update 07. Array of Objects.cpp

--- 07. Array of Objects.cpp
+++ 07. Array of Objects.cpp
@@ -25,3 +25,20 @@ int main() {
 
     return 0;
 }
+Output:
+
+Input:
+Enter details of 3 employees:
+Enter ID, Name, and Salary: 1 Raj 25000
+Enter ID, Name, and Salary: 2 Ravi 30000
+Enter ID, Name, and Salary: 3 Riya 28000
+
+output:
+ID: 1 | Name: Raj | Salary: 25000
+ID: 2 | Name: Ravi | Salary: 30000
+ID: 3 | Name: Riya | Salary: 28000
+
+
+Result
+Array of objects was successfully implemented.
+Multiple object data handled using loops
