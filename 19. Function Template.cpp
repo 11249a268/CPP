54983105ef23fcc021e5ec627d7a7e9806d8b115
11249a268 <11249a268@kanchiuniv.ac.in>
2025-11-20 22:48:25 +0530

    Update 19. Function Template.cpp

--- 19. Function Template.cpp
+++ 19. Function Template.cpp
@@ -11,3 +11,10 @@ int main() {
     cout << "Float Sum: " << add(2.5f, 3.5f) << endl;
     return 0;
 }
+Output:
+Max of 10 and 20 = 20
+Max of 5.5 and 2.3 = 5.5
+
+
+Result:
+Successfully implemented a *function template* that works for different data types.
